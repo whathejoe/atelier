cda55f6cc7ab40b96752f3e94acd4c36d700576e
whathejoe <clvillarealiv@gmail.com>
2015-08-30 04:01:58 +0800

    Integrated rails_admin

---
+++ db/migrate/20150829194815_create_products.rb
@@ -0,0 +1,11 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.decimal :price
+      t.string :description
+
+      t.timestamps null: false
+    end
+  end
+end
