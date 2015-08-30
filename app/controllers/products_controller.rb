374800c7fc82848d1204ffb08f1248d1e64a5764
whathejoe <clvillarealiv@gmail.com>
2015-08-31 05:52:15 +0800

    Image upload functional. Partial implementation of paperclip in views (/home, /store)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,15 @@
 class ProductsController < ApplicationController
   def home
+  	@bestsellers = Product.all
+  	@new_arrivals = Product.all
   end
 
   def store
+  	@products = Product.all
   end
 
   def product
+  	
   end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -8,6 +8,6 @@ class Product < ActiveRecord::Base
 		field :image
   	end
 
-  	has_attached_file :image, styles: { large: "600x600>", medium: "300x300", thumb: "150x150>" }
+  	has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150>" }
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
