bbf28029622c6404cb4afd03b942ce37ec421d9e
whathejoe <clvillarealiv@gmail.com>
2015-08-30 09:48:38 +0800

    Functioning login admin check and redirections

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,9 @@ class SessionsController < ApplicationController
 
 	def create
 	  @user = User.find_by_email(params[:session][:email])
-	  if @user && @user.authenticate(params[:session][:password])
+	  if @user && @user.authenticate(params[:session][:password]) && @user.is_admin?
 	    session[:user_id] = @user.id
-	    redirect_to '/'
+	    redirect_to '/admin'
 	  else
 	    redirect_to 'login'
 	  end 
